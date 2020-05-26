3a41fc7cf0edb549749b46c8d754d30e659d45a7
Ethan Moss <Ethanmoss2001@gmail.com>
2020-05-26 17:26:17 +0100

    Got articles working wth a get request

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const ArticleCard = ({article_id, title, votes, topic, author, created_at, comment_count}) => {
+
     return (
         <article className="eachArticle">
-            <h3>{title}<br></br>{author}<br></br>{topic}<br></br>{created_at}</h3>
+            <h3>{title}</h3>
+            <h4>{topic}<br></br>{author}<br></br>{created_at}<br></br>Total Comments: {comment_count}</h4>
+            <p className="votes">{votes}</p>
         </article>
     );
 };
